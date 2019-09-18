f2546984e0f25ede52554116ce4d1f6b42da3959
Lakshan Karunathilake <manualsplash@gmail.com>
2019-09-18 18:57:15 +0530

    Format and import angularfireauth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,63 @@
-import { UserServiceService } from './services/user-service.service';
-import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { UserServiceService } from "./services/user-service.service";
+import {
+  AngularFirestore,
+  AngularFirestoreModule
+} from "@angular/fire/firestore";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
-import { SecondComponent } from './second/second.component';
-import { FirstComponent } from './first/first.component';
-import {RouterModule, Routes} from "@angular/router";
-import {CustomMaterialModule} from "./core/material.module";
-import { LoginComponent } from './login/login.component';
-import { SettingComponent } from './setting/setting.component';
-import { HistoryComponent } from './history/history/history.component';
-import { AboutComponent } from './about/about.component';
-import { ProfileComponent } from './profile/profile.component';
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { SecondComponent } from "./second/second.component";
+import { FirstComponent } from "./first/first.component";
+import { RouterModule, Routes } from "@angular/router";
+import { CustomMaterialModule } from "./core/material.module";
+import { LoginComponent } from "./login/login.component";
+import { SettingComponent } from "./setting/setting.component";
+import { HistoryComponent } from "./history/history/history.component";
+import { AboutComponent } from "./about/about.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { FormsModule, ReactiveFormsModule, FormGroup } from "@angular/forms";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
 const appRoutes: Routes = [
-  { path: '', component: FirstComponent, data: { title: 'First Component' } },
-  { path: 'first', component: FirstComponent, data: { title: 'First Component' } },
-  { path: 'second', component: SecondComponent, data: { title: 'Second Component' } },
-  { path: 'setting', component: SettingComponent, data: { title: 'Setting Component' } },
-  { path: 'history', component: HistoryComponent, data: { title: 'History Component' } },
-  { path: 'about', component: AboutComponent, data: { title: 'About Component' } },
-  { path: 'profile', component: ProfileComponent, data: { title: 'Profile Component' } }
-
+  { path: "", component: FirstComponent, data: { title: "First Component" } },
+  {
+    path: "first",
+    component: FirstComponent,
+    data: { title: "First Component" }
+  },
+  {
+    path: "second",
+    component: SecondComponent,
+    data: { title: "Second Component" }
+  },
+  {
+    path: "setting",
+    component: SettingComponent,
+    data: { title: "Setting Component" }
+  },
+  {
+    path: "history",
+    component: HistoryComponent,
+    data: { title: "History Component" }
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+    data: { title: "About Component" }
+  },
+  {
+    path: "profile",
+    component: ProfileComponent,
+    data: { title: "Profile Component" }
+  }
 ];
 @NgModule({
   declarations: [
@@ -53,17 +80,14 @@ const appRoutes: Routes = [
       { useHash: true } // <-- debugging purposes only
     ),
     CustomMaterialModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebase),
-        AngularFireDatabaseModule,
-        AngularFirestoreModule,
-        ReactiveFormsModule,
-        FormsModule,
-        
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [
-    AngularFirestore,
-    UserServiceService
-  ],
+  providers: [AngularFirestore, UserServiceService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
