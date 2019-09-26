fd9f48821b050d9fb5d643e779a0880f2bc1f090
Tharinduyasarathna27 <tharinduyasarathna@gmail.com>
2019-09-26 22:28:53 +0530

    add profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserServiceService } from "../services/user-service.service";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
+  users: any;
 
-  constructor() { }
+  constructor(private userservice: UserServiceService) {}
 
   ngOnInit() {
+    
   }
 
+  UpdateRecord(recordRow) {
+    let record = {};
+    record["name"] = recordRow.EditName;
+    record["email"] = recordRow.EditEmail;
+    record["password"] = recordRow.EditPassword;
+    record["Phone"] = recordRow.EditPhone;
+    this.userservice.updateUser(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
 }
