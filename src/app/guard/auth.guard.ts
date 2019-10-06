e71814114aa5d799d11a7d0ebe30feeca08af84d
Tharinduyasarathna27 <tharinduyasarathna@gmail.com>
2019-10-07 00:46:41 +0530

    update auth guard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -2,26 +2,28 @@ import { AuthService } from './../services/auth/auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { NotifierService } from 'angular-notifier';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(private router: Router, private authservice: AuthService){}
+  private  notifier: NotifierService;
+  constructor(private router: Router, private authservice: AuthService, notifierService: NotifierService){this.notifier = notifierService;}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if ( this.authservice.isLoggedIn()){
+    if (this.authservice.isLoggedIn()){
          
          console.log("true");
          return true
        } else  {        
          // Hands the user to the LogIn page 
-         
+         this.notifier.notify( 'error', "You are currently not logged in, please provide Login!" );
          this.router.navigate( ["login"] );
-         alert("You are currently not logged in, please provide Login!")
+        // alert("You are currently not logged in, please provide Login!")
+        
          return false
  
        }
